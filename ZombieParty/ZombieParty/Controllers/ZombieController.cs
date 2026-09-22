ec7f17fec4bea3a04b82948dbd890fa86fabbfed
JAlexanderSN <2558467@cegepmontpetit.ca>
2026-09-22 15:16:40 -0400

    FCT Zombie Edit
    Ajout de la View Edit et des actions dans ZombieController

--- ZombieParty/ZombieParty/Controllers/ZombieController.cs
+++ ZombieParty/ZombieParty/Controllers/ZombieController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ZombieParty.Models;
 using ZombieParty.Models.Data;
+using ZombieParty.ViewModels;
 
 namespace ZombieParty.Controllers
 {
@@ -41,7 +42,21 @@ namespace ZombieParty.Controllers
             return View(zombieVM);
         }
 
+        public IActionResult Edit(int id)
+        {
+            ZombieVM zombieVM = new ZombieVM();
+            zombieVM.Zombie = _baseDonnees.Zombies.Find(id);
+            zombieVM.ZombieTypeSelectList = _baseDonnees.ZombieTypes.Select(t => new SelectListItem
+            {
+                Text = t.TypeName,
+                Value = t.Id.ToString()
+            }).OrderBy(t => t.Text);
+
+            return View(zombieVM);
+        }
+
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Create(ZombieVM zombieVM)
         {
             //Si le modèle est valide le zombie est ajouté et nous sommes redirigé vers index.
@@ -50,6 +65,29 @@ namespace ZombieParty.Controllers
                 _baseDonnees.Zombies.Add(zombieVM.Zombie);
                 _baseDonnees.SaveChanges();
                 TempData["Success"] = $"Zombie {zombieVM.Zombie.Name} added";
+                return this.RedirectToAction("Index");
+            }
+            zombieVM.ZombieTypeSelectList = _baseDonnees.ZombieTypes.Select(t => new SelectListItem
+            {
+                Text = t.TypeName,
+                Value = t.Id.ToString()
+            }).OrderBy(t => t.Text);
+
+            return View(zombieVM);
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ZombieVM zombieVM)
+        {
+            //Si le modèle est valide le zombie est ajouté et nous sommes redirigé vers index.
+            if (ModelState.IsValid)
+            {
+                _baseDonnees.Zombies.Update(zombieVM.Zombie);
+                _baseDonnees.SaveChanges();
+                TempData["Success"] = $"Zombie {zombieVM.Zombie.Name} fue modificado";
                 return this.RedirectToAction("Index");
             }
             zombieVM.ZombieTypeSelectList = _baseDonnees.ZombieTypes.Select(t => new SelectListItem
